75934f7bab975057265e9067588d5b317f954f6a
Leena Kaikkonen <Leena.Kaisa.Maria.Kaikkonen@student.laurea.fi>
2021-05-09 19:21:28 +0300

    pages and navigation added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,46 @@
 import { NgModule } from '@angular/core';
+import { SplashComponent } from './splash/splash.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { FourComponent } from './four/four.component';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'splash',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'splash',
+    component: SplashComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: 'one',
+    component: OneComponent
+  },
+  {
+    path: 'two',
+    component: TwoComponent
+  },
+  {
+    path: 'three',
+    component: ThreeComponent
+  },
+  {
+    path: 'four',
+    component: FourComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
